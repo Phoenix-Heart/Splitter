e73e239d7c5291c7a34b4ee4a58fcefada03c8e5
chd <lady_aviendha@hotmail.com>
2013-06-30 17:40:25 -0600

    add support for answer key
    Program now outputs all code lines as answers; also now allows questions
    to be separated by "!" and includes documentation

--- Run.java
+++ Run.java
@@ -1,91 +1,146 @@
+/* Simple code to extract some quiz questions and answers.
+ * Comments added liberally to aid readers who are new to Java or to programming
+ * 
+ * Imports
+ * IO imports file input/output for reading and writing.
+ * Scanner is used to read command line if file selector option fails.
+ * awt and swing handle file opening GUI elements
+ */
+
 import java.awt.Panel;
 import java.io.*;
 import java.util.Scanner;
-
 import javax.swing.JFileChooser;
 
-
+// As a natively OOP language, everything that executes must run inside of a class
 public class Run {
 
-
+	// main is required and always executes first. args are not used in this program, but they allow for command line inputs if used.
 	public static void main(String[] args) {
-		convert(selector());
+		convert(selector());	// this main code is pretty short, it just calls two static methods.
+								// a static method is just a function that doesn't require an object.
+	}
+	
+	//append to a given file name
+	static String appendname(String name, String append) {
+		  return name.replaceFirst(".txt", append+".txt");	// in java, strings are objects too so we have to call a method to regex (replace text with some other text)
 	}
 	static String selector() {
-		String filename;
-		JFileChooser fc = new JFileChooser();
-		int returnVal = fc.showOpenDialog(new Panel()); //Where frame is the parent component
+		String filename;	// this is the filename of our selected file. we have to declare variables in Java. This variable is currently null. it points to nothing.
+		JFileChooser fc = new JFileChooser();			// Java's libraries allow us to easily create premade GUI elements. 
+		int returnVal = fc.showOpenDialog(new Panel()); // We create a basic panel to build our dialog on top of. This is the only element in our panel but panels can hold more than one thing.
 
-		File file = null;
-		if (returnVal == JFileChooser.APPROVE_OPTION) {
+		File file;
+		if (returnVal == JFileChooser.APPROVE_OPTION) {		// this allows the code to only run if the user selects and accepts a file.
 		    file = fc.getSelectedFile();
-		    //Now you have your file to do whatever you want to do
-		    filename = file.getAbsolutePath();
+		    												//Now you have your file to do whatever you want to do
+		    filename = file.getAbsolutePath();				
 		} else {
-			System.out.println("File selector failed.");
+			System.out.println("File selector failed.");	// Java's method to print to the command line
 			System.out.println("Enter filename");
-			Scanner in = new Scanner(System.in);
-			filename = in.next();
+			Scanner in = new Scanner(System.in);			// reads from the command line, in case the user fails to select a file.
+			filename = in.next();							// read and save the next string, does not allow whitespace.
 		}
 		return filename;
 		
 	}
-	static void convert(String filename) {
-
-			  
-			  String returnValue = "";
+	static void convert(String filename) {	
+				// built in IO classes help us read and write from our files
 			  FileReader infile = null;
 			  FileWriter outfile = null;
+			  FileWriter ansfile = null;
 			  
 			  try {
-				  String outfilename = filename.replaceFirst(".txt", "_clean.txt");
+				  // create new files with similar names to the original file
+				  String outfilename = appendname(filename, "_clean");
+				  String ansfilename = appendname(filename, "_ans");
+				  
+				  // more IO initialization
 			      outfile = new FileWriter(outfilename);
-			      BufferedWriter writer = new BufferedWriter(outfile);
-			      //writer.write(s);
+			      ansfile = new FileWriter(ansfilename);
+			      BufferedWriter writer = new BufferedWriter(outfile);		
+			      BufferedWriter awriter = new BufferedWriter(ansfile);
+			      
+			      // checking for existing directory.. strictly speaking this isn't necessary here but it is included for the sake of example.
 			      File file = new File(outfilename);
 			      File parent = file.getParentFile();
-
+			      
+			      // attempt to make the directory if it does not exist.
 			      if(!parent.exists() && !parent.mkdirs()){									// !parent.mkdirs() -> if it fails to create the directory
 			          throw new IllegalStateException("Couldn't create dir: " + parent);
 			      } 
-				  
+			      
+				// more IO initialization
 			    infile = new FileReader(filename);
 			    BufferedReader reader = new BufferedReader(infile);
+			    
 			    String line = "";
+			    int w1 = 0,w2 = 0;
+			    int qNum = 1;
+			    boolean nextq = true;
+			    int quiz = 1;
+			    boolean ansdone = false;
+			    
+			    // read in one line at a time until we reach the end of the file
 			    while ((line = reader.readLine()) != null) {
+			    	
+			    	// ignore empty lines
 			    	if(!line.isEmpty()) {
 			    		
-				    	while(line.charAt(0)==' ') {		// strip out leading space
+			    		// strip out leading space
+				    	while(line.charAt(0)==' '&&line.length()>1) {		
 				    		line = line.substring(1);
 				    	}
+				    	
+				    	// store comments as questions
 				    	if(line.charAt(0)=='#') {
-				    		System.out.print("writing..");
+				    		
+				    		// count the number of lines being written to the file. ++ is the same as w = w+1 or w += 1
+				    		w1++;
+				    		// write the line and start a new one: exclude the # tag
 				    		writer.write(line.substring(1));
 				    		writer.newLine();
-				    	}// remove hash
-				    	if(line.startsWith("Quiz")) {
-				    		System.out.print("writing..");
-				    			writer.write(line);
+				    	}// remove hash and write the questions
+				    	else {
+				    		ansdone = true;			// lets program know that the current question will be finished once we reach new #
+				    		// count the number of lines being written to the file
+				    		w2++;
+				    		// write the line and start a new one
+				    		awriter.write(line);
+				    		awriter.newLine();
+				    	}	// write the answers
+				    	
+				    	//recognize a new question if the line starts with 'Quiz' or '!'
+				    	if((line.startsWith("Quiz")||line.startsWith("!"))) {
+				    		// count the number of lines being written to the file
+				    		w1++;
+				    		w2++;
+				    		String q = "Question #"+qNum;
+				    		// write the line and start a new one
+				    			writer.write(q);
 				    			writer.newLine();
+					    		awriter.write(q);
+					    		awriter.newLine();
+					    	qNum++;
 				    	}
 			    	}
 			    }
-			    writer.close();
+			    writer.close();			// files, io streams, and buffers must be closed
 			  } catch (Exception e) {
-			      throw new RuntimeException(e);
-			  } finally {
+			      throw new RuntimeException(e);	// I/O create exceptions that Java requires us to catch. 
+			  } finally {							// These will display to the user if something fails during runtime.
 			    if (infile != null) {
 			      try {
 			        infile.close();
 			      } catch (IOException e) {
-			        System.out.println("Error: did not close file properly.");
+			        System.out.println("Error: did not close file properly.");	// we can also choose to create our own messages during an exception.
 			      }
 			    }
 			    if (outfile != null) {
 		        try {
 		            outfile.close();
 		          } catch (IOException e) {
-		            // Ignore issues during closing
+		            // Ignores issues during closing
 		          }
 			  }
 			  }
